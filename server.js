01d17ff9e69aa81fa0da1d53f6f94590844ac78e
linda <lindaochem@gmail.com>
2023-11-01 10:15:56 +0100

    ci2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8081;
 
 app.get('/', (req, res) => {
-  res.send('simple Node.js server written with my pc on my azure account please reflect on azure now!');
+  res.send('simple Node.js server written with my pc on my azure account please reflect on azure!');
 });
 
 app.listen(PORT, () => {
